cb3cfbd67e3b3c537f4131a083b055ae9b1478a8
Deepu Mohan Puthrote <git@deepumohan.com>
2023-07-16 22:25:06 +0100

    Remove welcome message

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import Counter from './components/Counter.vue'
 </script>
 
 <template>
@@ -9,7 +8,7 @@ import Counter from './components/Counter.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Vue Notes" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -17,8 +16,6 @@ import Counter from './components/Counter.vue'
       </nav>
     </div>
   </header>
-
-  <Counter />
 
   <RouterView />
 </template>
--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ClickCount from '@/components/ClickCount.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ClickCount />
   </main>
 </template>
