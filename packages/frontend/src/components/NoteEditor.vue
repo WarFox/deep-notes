5b4829e7dff34ea0aaac6544aa804528422b6cd1
Deepu Mohan Puthrote <git@deepumohan.com>
2023-07-22 00:09:10 +0100

    Setup quill editor; publish and subscribe to deltas via ably channel 💥

--- packages/frontend/src/components/NoteEditor.vue
+++ packages/frontend/src/components/NoteEditor.vue
@@ -1,22 +1,71 @@
 <template>
-    <QuillEditor @textChange="handleChange" :options="options" />
+  <main>
+    <QuillEditor
+      ref="editor"
+      content-type="delta"
+      @textChange="handleTextChange"
+      @ready="handleReady"
+      :options="options"
+    />
+  </main>
 </template>
 
 <script setup lang="ts">
+import { QuillEditor, Delta, Quill } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { ref, computed, onMounted, watch } from 'vue'
+import { Types, Realtime } from 'ably'
 
-import { QuillEditor } from '@vueup/vue-quill';
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import { useRealtimeStore } from '@/stores/realtime'
 
 const options = {
-    placeholder: 'Start collaborating! :tada:',
-    theme: 'snow'
+  placeholder: 'Start collaborating! :tada:',
+  theme: 'snow',
 }
 
-function handleChange(change: {
-    delta: any,
-    oldContents: any,
-    source: any
-}) {
-    console.log(change.delta);
+// The ref=editor in QuillEditor works like magic!
+const editor = ref(null)
+const quill = ref<Quill>(null)
+
+const store = useRealtimeStore()
+const channel = computed(() => store.channel)
+const isChannelAttached = computed(() => store.isChannelAttached)
+
+interface TextChange {
+  delta: Delta
+  oldContents: Delta
+  source: Sources
 }
+
+// TODO set the content of editor with content saved in database
+function handleReady() {
+  quill.value = editor.value.getQuill()
+}
+
+function handleTextChange(change: TextChange) {
+  // only publish changes made by the user
+  if (change.source == 'user' && channel.value) {
+    channel.value.publish('delta', change.delta)
+  }
+}
+
+// Subscribe to channel only after channel is attached
+watch(isChannelAttached, (isAttached) => {
+  if (isAttached) {
+    channel.value.subscribe('delta', (message: Types.Message) => {
+      if (message.clientId !== store.ablyClientId) {
+        quill.value.updateContents(message.data)
+      }
+    })
+  }
+})
+
+onMounted(() => {
+  store.initializeAbly()
+})
+
+/* References
+   https://quilljs.com/docs/api/#events
+   https://github.com/vueup/vue-quill/issues/188
+*/
 </script>
---
+++ packages/frontend/src/stores/realtime.ts
@@ -0,0 +1,62 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+import { Realtime, Types } from 'ably'
+
+export const useRealtimeStore = defineStore('realtime', () => {
+  const authUrl = ref(`${import.meta.env.VITE_APP_API_URL}/ably-token`)
+
+  const isConnected = ref(false)
+  const ablyRealtimeClient = ref<Realtime>(null)
+  const ablyClientId = ref('noclientid')
+
+  const channelName = 'editor'
+  const channel = ref(null)
+
+  const isChannelAttached = ref(false)
+
+  function attachAblyChannel() {
+    channel.value = ablyRealtimeClient.value.channels.get(channelName, {
+      // params: { rewind: '2m' }
+    })
+
+    isChannelAttached.value = true
+  }
+
+  async function initializeAbly() {
+    if (!isConnected.value) {
+      const clientOptions: Types.ClientOptions = {
+        authUrl: authUrl.value,
+        authMethod: 'POST'
+        // log: { level: 4 }
+      }
+
+      const realtime = new Realtime(clientOptions)
+
+      realtime.connection.on('connected', () => {
+        isConnected.value = true
+        ablyClientId.value = realtime.auth.clientId
+        ablyRealtimeClient.value = realtime
+
+        attachAblyChannel()
+      })
+
+      realtime.connection.on('disconnected', () => {
+        isConnected.value = false
+      })
+    }
+  }
+
+  function closeAblyConnection() {
+    console.debug('closing abyl connection')
+    ablyRealtimeClient.value.connection.close()
+  }
+
+  return {
+    channel,
+    initializeAbly,
+    isChannelAttached,
+    ablyClientId,
+    closeAblyConnection
+  }
+})
