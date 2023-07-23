3125a4411be467b7cc6fe13de06c07d5d657ceaa
Deepu Mohan Puthrote <git@deepumohan.com>
2023-07-23 12:39:45 +0100

    Setup tailwind colors 🎉

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      primary: colors.sky,
+      secondary: colors.yellow,
+      neutral: colors.gray
+    }
   },
   plugins: []
 }
