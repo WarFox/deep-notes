f25501601d2c30a77bd3bcbd8634df1f205cde33
Deepu Mohan Puthrote <git@deepumohan.com>
2023-07-24 08:17:00 +0100

    Show cursors for each participant

--- packages/frontend/src/components/NoteEditor.vue
+++ packages/frontend/src/components/NoteEditor.vue
@@ -20,6 +20,7 @@
         @selectionChange="handleSelectionChange"
         @ready="handleReady"
         :options="options"
+        :modules="modules"
       />
     </div>
   </main>
@@ -28,6 +29,7 @@
 <script setup lang="ts">
 import { QuillEditor, Delta, Quill } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import QuillCursors from 'quill-cursors'
 import { ref, computed, onMounted, onUnmounted, watch } from 'vue'
 import { Types, Realtime } from 'ably'
 import LoadingIndicator from '@/components/LoadingIndicator.vue'
@@ -35,9 +37,13 @@ import AvatarStacks from '@/components/AvatarStacks.vue'
 
 import { useRealtimeStore } from '@/stores/realtime'
 
+// console.log('register quill')
+// Quill.register('modules/cursor', QuillCursors)
+
 // The ref=editor in QuillEditor works like magic!
 const editor = ref(null)
 const quill = ref<Quill>(null)
+const cursorModule = ref(null)
 
 const store = useRealtimeStore()
 const channel = computed(() => store.channel)
@@ -48,8 +54,58 @@ const isLoading = computed(() => !isConnected)
 const options = {
   placeholder: 'Start collaborating! :tada:',
   theme: 'snow',
-  readOnly: !isConnected
+  readOnly: !isConnected,
+  modules: {
+    toolbar: true,
+    cursors: true
+  }
 }
+
+const modules = {
+  name: 'cursors',
+  module: QuillCursors
+}
+
+// add/remove as participants change
+const participants = computed(() => store.participants.values())
+watch(participants, (newParticipant) => {
+  // console.log('participants updated')
+  // console.log(participants)
+  // console.log(newParticipant)
+  if (cursorModule.value) {
+    for (let [key, value] of store.participants) {
+      const { clientId, color } = value
+      console.log('create cursor', clientId)
+      cursorModule.value.createCursor(clientId, clientId, color)
+    }
+  }
+})
+
+// watch(participants, (newParticipant) => {
+//   console.log('participants changed', participants.value)
+//   console.log(newParticipant.next())
+//   if (cursorModule.value) {
+//     participants.forEach((participant) => {
+//       console.log(participant)
+//       const { color, range } = participant
+//       cursorModule.value.createCursor(clientId, clientId, color)
+//       if (range) {
+//         cursorsModule.moveCursor(clientId, range)
+//       }
+//       //             result.push(participant)
+//     })
+//   }
+// })
+// computed(() => {
+//     const result = []
+//     if (store.participants.value) {
+//         store.participants.value.forEach((participant, clientId) => {
+//             console.log(clientId, participant)
+//             result.push(participant)
+//         })
+//     }
+//     return result
+// })
 
 interface TextChange {
   delta: Delta
@@ -60,6 +116,10 @@ interface TextChange {
 // TODO set the content of editor with content saved in database
 function handleReady() {
   quill.value = editor.value.getQuill()
+
+  cursorModule.value = quill.value.getModule('cursors')
+  cursorModule.value.clearCursors()
+  cursorModule.value.createCursor('one', 'one', 'red')
 }
 
 function handleTextChange(change: TextChange) {
