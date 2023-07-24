dc086064044878481f7c48dc1f24327173e69889
Deepu Mohan Puthrote <git@deepumohan.com>
2023-07-25 00:55:43 +0100

    APIs for creating, listing and getting note ready 🎉

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -1,12 +1,32 @@
 import { Notes } from "@deep-notes/core/notes";
+import {
+  APIGatewayProxyResultV2,
+  APIGatewayProxyHandlerV2,
+  APIGatewayProxyEventV2,
+} from "aws-lambda";
 
-export async function handler() {
-  return {
-    statusCode: 201,
-    body: Notes.create({
-      createdBy: "some user id",
-      title: "newtitel",
-      content: "some content",
-    }),
-  };
-}
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyResultV2> => {
+  try {
+    const { body } = event;
+
+    const data = JSON.parse(body);
+
+    const result = await Notes.create(data);
+
+    return {
+      statusCode: 201,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(result),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "some error happened while creating a note",
+      }),
+    };
+  }
+};
--- packages/functions/src/get.ts
+++ packages/functions/src/get.ts
@@ -1,8 +1,34 @@
 import { Notes } from "@deep-notes/core/notes";
+import {
+  APIGatewayProxyResultV2,
+  APIGatewayProxyHandlerV2,
+  APIGatewayProxyEventV2,
+} from "aws-lambda";
 
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: Notes.get("someid"),
-  };
-}
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyResultV2> => {
+  try {
+    const noteId = event?.pathParameters?.id;
+
+    if (noteId) {
+      const result = await Notes.find(noteId);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result),
+      };
+    }
+
+    return {
+      statusCode: 200,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "some error happened while listing notes",
+      }),
+    };
+  }
+};
