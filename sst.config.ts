815b861ed0b186b84346fdedcacb42c0e9f15d00
Deepu Mohan Puthrote <git@deepumohan.com>
2023-07-24 14:49:40 +0100

    Add Auth to the stack! 🐛 🚋

--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,17 @@
 import { SSTConfig } from "sst";
 import { Api } from "./stacks/Api";
-import { Web } from "./stacks/Web";
+import { Auth } from "./stacks/Auth";
 import { Database } from "./stacks/Database";
+import { Web } from "./stacks/Web";
 
 export default {
-  config(_input) {
-    return {
-      name: "deep-notes",
-      region: "eu-west-1",
-    };
-  },
-  stacks(app) {
-    app.stack(Database).stack(Api).stack(Web);
+    config(_input) {
+        return {
+            name: "deep-notes",
+            region: "eu-west-1",
+        };
+    },
+    stacks(app) {
+        app.stack(Database).stack(Auth).stack(Api).stack(Web);
   },
 } satisfies SSTConfig;
