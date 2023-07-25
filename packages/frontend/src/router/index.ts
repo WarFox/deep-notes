54c7c170914e8a5afa86de2829eab9c144caf842
Deepu Mohan Puthrote <git@deepumohan.com>
2023-07-25 02:24:02 +0100

    Redirect to home if not authenicated

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotesView from '../views/NotesView.vue'
 import EditorView from '../views/EditorView.vue'
+import { useAuthenticator } from '@aws-amplify/ui-vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,6 +31,20 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     }
   ]
+})
+
+const store = useAuthenticator()
+
+router.beforeEach(async (to, from) => {
+  if (
+    // make sure the user is authenticated
+    store.route !== 'authenticated' &&
+    // ❗️ Avoid an infinite redirect
+    to.name !== 'home'
+  ) {
+    // redirect the user to the login page
+    return { name: 'home' }
+  }
 })
 
 export default router
